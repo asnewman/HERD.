338597a5ea563506f426365514da6b62663db989
Jordin Gardner <gard.jordin@gmail.com>
2023-04-16 16:09:28 -0700

    SHEEP

--- game.js
+++ game.js
@@ -19,13 +19,35 @@ const SPRITES = {
   base: "base",
   path: "path",
   empty: "empty",
+  grassTile: "grassTile",
 };
 
 // load sprites
-loadSprite(SPRITES.sheep, "sprites/lolsheep.jpg");
 loadSprite(SPRITES.base, "sprites/base.png");
 loadSprite(SPRITES.path, "sprites/path.png");
 loadSprite(SPRITES.empty, "sprites/empty.png");
+
+loadSprite(SPRITES.sheep, "sprites/spritesheet-sheep.png", {
+  sliceX: 3,
+  sliceY: 2,
+  anims: {
+    graze: {
+      loop: true,
+      from: 0,
+      to: 5,
+    },
+  },
+});
+
+loadSpriteAtlas("sprites/spritesheet-env.png", {
+  [SPRITES.grassTile]: {
+    x: 16,
+    y: 16,
+    width: 64,
+    height: 32,
+    sliceX: 2,
+  },
+});
 
 const SCENES = {
   menu: "menu",
@@ -78,10 +100,33 @@ scene(SCENES.mapGeneration, () => {
 });
 
 scene(SCENES.sheepConfig, () => {
-  createSheep({
-    name: "sheep1",
-    pos: vec2(0, 0),
-  });
+  // temporary: cover the screen with grass
+  add([
+    sprite(SPRITES.grassTile, {
+      width: width(),
+      height: height(),
+      tiled: true,
+    }),
+    pos(0, 0),
+  ]);
+
+  // temporary: create a bunch of sheep in random positions
+  const segmentWidth = width() / 5;
+  const segmentHeight = height() / 5;
+  const getOffset = () => rand(-50, 50);
+  for (let x = 0; x < 5; x++) {
+    for (let y = 0; y < 5; y++) {
+      const xStart = x * segmentWidth;
+      const yStart = y * segmentHeight;
+      createSheep({
+        name: `sheep${x}${y}`,
+        pos: [
+          xStart + segmentWidth / 2 + getOffset(),
+          yStart + segmentHeight / 2 + getOffset(),
+        ],
+      });
+    }
+  }
 });
 
 go(SCENES.menu);
@@ -90,6 +135,11 @@ const SHEEP_STATES = {
   grazing: "grazing",
   walking: "walking",
 };
+
+const SHEEP_ANIM_SPEED = 0.6;
+
+const SHEEP_GRAZE_VELOCITY = 3000;
+const SHEEP_GRAZE_DIRECTION_CHANGE_TIME = 2;
 
 function createSheep(options) {
   if (!options.name) {
@@ -106,9 +156,9 @@ function createSheep(options) {
 
   const sheep = add([
     options.name,
-    options.pos,
-    sprite(SPRITES.sheep),
-
+    pos(...options.pos),
+    sprite(SPRITES.sheep, { animSpeed: SHEEP_ANIM_SPEED }),
+    scale(3, 3),
     state(
       options.initialState || SHEEP_STATES.grazing,
       Object.values(SHEEP_STATES)
@@ -116,13 +166,92 @@ function createSheep(options) {
     area(),
   ]);
 
-  sheep.onStateEnter(SHEEP_STATES.grazing, () => {
-    // recursively:
-    // - play grazing animation
-    // - randomly shuffle around
+  const sheepState = {
+    grazing: {
+      /**
+       * The current direction the sheep is moving in.
+       * Can be "left", "right", or "idle".
+       */
+      direction: "right",
+      /**
+       * The amount of time for which the sheep has been moving in the
+       * current direction.
+       */
+      cycleTime: 0,
+      /**
+       * The amount of time for which the sheep should move in the current
+       * direction before changing it.
+       */
+      cycleTimeLimit: 0,
+    },
+  };
+
+  const getDirectionTimeLimit = () => rand(10) * 0.2;
+
+  sheep.onStateEnter(SHEEP_STATES.grazing, async () => {
+    sheepState.grazing = {
+      lastDirection: "right",
+      direction: ["left", "right", "idle"][rand(2)],
+      cycleTime: 0,
+      cycleTimeLimit: getDirectionTimeLimit(),
+    };
+    sheep.flipX(sheepState.grazing.direction === "left");
+    sheep.play("graze");
   });
 
-  setTimeout(() => alert("baaaa"));
+  sheep.onStateUpdate(SHEEP_STATES.grazing, () => {
+    const delta = dt();
+
+    let moveValues = [];
+    switch (sheepState.grazing.direction) {
+      case "idle": {
+        moveValues = [0, 0];
+        break;
+      }
+      case "left": {
+        moveValues = [-SHEEP_GRAZE_VELOCITY * delta, 0];
+        break;
+      }
+      case "right": {
+        moveValues = [SHEEP_GRAZE_VELOCITY * delta, 0];
+        break;
+      }
+    }
+    sheep.move(...moveValues);
+
+    sheepState.grazing.cycleTime += delta;
+
+    const shouldNotChangeDirection =
+      sheepState.grazing.cycleTime <= sheepState.grazing.cycleTimeLimit;
+    if (shouldNotChangeDirection) {
+      return;
+    }
+
+    sheepState.grazing.cycleTime = 0;
+    sheepState.grazing.cycleTimeLimit = getDirectionTimeLimit();
+
+    // if currently idle, start moving in a direction
+    if (sheepState.grazing.direction === "idle") {
+      // if the sheep was last going left, go right now
+      sheepState.grazing.direction =
+        sheepState.grazing.lastDirection === "right" ? "left" : "right";
+      // track last direction to know what direction to move in next time
+      sheepState.grazing.lastDirection = sheepState.grazing.direction;
+      // sheep sprite faces right by default - flip it to the left if it's going left
+      sheep.flipX(sheepState.grazing.direction === "left");
+      // play the grazing animation while moving
+      sheep.play("graze");
+      return;
+    }
+
+    // sheep was moving, so now should idle for a cycle
+    sheepState.grazing.direction = "idle";
+    sheep.stop();
+  });
+
+  sheep.enterState(SHEEP_STATES.grazing);
+
+  gameState.sheep[options.name] = sheep;
 
   return sheep;
 }
