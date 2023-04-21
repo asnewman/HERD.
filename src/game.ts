0edcde053a9b52b07404d9723b75e2553ae0fb29
Jordin Gardner <gard.jordin@gmail.com>
2023-04-20 23:27:57 -0700

    sheep to own module

--- src/game.ts
+++ src/game.ts
@@ -1,46 +1,39 @@
-import { GameObj } from "kaboom";
-
 import { k } from "./kaboom";
 import { addButton } from "./lib";
+import { createSheep } from "./sheep";
+
+export interface IGameState {
+  /**
+   * A map of sheep by sheepName -> Sheep object.
+   * When a sheep is added to the game, it will be added to this map,
+   * and we'll ensure that its name is unique.
+   */
+  sheep: Record<string, any>;
+  /**
+   * A set of the names of the sheep that are currently selected.
+   * Set<string>
+   */
+  sheepSelected: Set<string>;
+}
+
+export const SPRITES = {
+  sheep: "sheep",
+  sheepBomber: "sheepBomber",
+  baseTopRight: "baseTopRight",
+  baseTopLeft: "baseTopLeft",
+  baseBottomRight: "baseBottomRight",
+  baseBottomLeft: "baseBottomLeft",
+  baseVertical: "baseVertical",
+  baseHorizontal: "baseHorizontal",
+  path: "path",
+  empty: "empty",
+  grassTile: "grassTile",
+};
 
 export function startGame() {
-  /**
-   * Global state tracking the current state of the game. I think
-   * an object is probably ok for now, but lmk if you disagree or think
-   * we should take a different approach!
-   */
-  var gameState: {
-    sheep: {
-      // TODO: type the sheep
-      [sheepName: string]: any;
-    };
-    sheepSelected: Set<string>;
-  } = {
-    /**
-     * A map of sheep by sheepName -> Sheep object.
-     * When a sheep is added to the game, it will be added to this map,
-     * and we'll ensure that its name is unique.
-     */
+  let gameState: IGameState = {
     sheep: {},
-    /**
-     * A set of the names of the sheep that are currently selected.
-     * Set<string>
-     */
     sheepSelected: new Set(),
-  };
-
-  const SPRITES = {
-    sheep: "sheep",
-    sheepBomber: "sheepBomber",
-    baseTopRight: "baseTopRight",
-    baseTopLeft: "baseTopLeft",
-    baseBottomRight: "baseBottomRight",
-    baseBottomLeft: "baseBottomLeft",
-    baseVertical: "baseVertical",
-    baseHorizontal: "baseHorizontal",
-    path: "path",
-    empty: "empty",
-    grassTile: "grassTile",
   };
 
   // load sprites
@@ -71,6 +64,7 @@ export function startGame() {
   k.loadSpriteAtlas("sprites/spritesheet-sheep.png", {
     [SPRITES.sheep]: sheepAtlasSettings,
   });
+
   k.loadSpriteAtlas("sprites/spritesheet-sheep-bomber.png", {
     [SPRITES.sheepBomber]: sheepAtlasSettings,
   });
@@ -192,7 +186,7 @@ export function startGame() {
       for (let y = 0; y < 5; y++) {
         const xStart = x * segmentWidth;
         const yStart = y * segmentHeight;
-        createSheep({
+        createSheep(gameState, {
           name: `sheep${x}${y}`,
           pos: [
             xStart + segmentWidth / 2 + getOffset(),
@@ -252,211 +246,6 @@ export function startGame() {
       onClick: onSheepTypeClick("commando"),
     });
   });
-
-  enum SheepState {
-    grazing = "grazing",
-    walking = "walking",
-  }
-
-  const SHEEP_ANIM_SPEED = 0.6;
-  const SHEEP_GRAZE_VELOCITY = 3000;
-
-  interface ICreateSheepOptions {
-    name: string;
-    pos: [number, number];
-    initialState?: SheepState;
-  }
-
-  function createSheep(options: ICreateSheepOptions) {
-    if (!options.name) {
-      throw new Error("Sheep must have a name");
-    }
-
-    if (gameState.sheep[options.name]) {
-      throw new Error(`Sheep name ${options.name} already exists`);
-    }
-
-    if (!options.pos) {
-      throw new Error("Sheep must have a position");
-    }
-
-    function selectable() {
-      let isSelected = false;
-      return {
-        id: "selectable",
-        require: ["area"],
-        draw() {
-          if (!isSelected) return;
-          k.drawRect({
-            width: 32,
-            height: 17,
-            pos: k.vec2(0, 0),
-            fill: false,
-            outline: { color: k.RED, width: 1 },
-          });
-        },
-        inspect() {
-          return String(isSelected);
-        },
-        setSelected(value: boolean) {
-          isSelected = value;
-        },
-        setType(this: GameObj, type: "bomber" | "shielder" | "commando") {
-          if (type === "bomber") {
-            this.use(k.sprite(SPRITES.sheepBomber));
-            return;
-          }
-
-          this.use(k.sprite(SPRITES.sheep));
-        },
-        toggleSelected() {
-          const wasAlreadySelected = gameState.sheepSelected.has(sheep.name);
-
-          wasAlreadySelected
-            ? gameState.sheepSelected.delete(sheep.name)
-            : gameState.sheepSelected.add(sheep.name);
-
-          this.setSelected(!wasAlreadySelected);
-        },
-      };
-    }
-
-    const sheepTag = `sheep-${options.name}`;
-    const sheep = k.add([
-      "sheep",
-      { name: options.name },
-      sheepTag,
-      k.pos(...options.pos),
-      k.sprite(SPRITES.sheep, { animSpeed: SHEEP_ANIM_SPEED }),
-      k.scale(3, 3),
-      k.state(
-        options.initialState || SheepState.grazing,
-        Object.values(SheepState)
-      ),
-      k.area(),
-      selectable(),
-      k.body(),
-    ]);
-
-    const sheepState = {
-      grazing: {
-        /**
-         * The last direction the sheep was moving in.
-         * Used to determine what the next direction should be.
-         */
-        lastDirection: "",
-        /**
-         * The current direction the sheep is moving in.
-         * Can be "left", "right", or "idle".
-         */
-        direction: "right",
-        /**
-         * The amount of time for which the sheep has been moving in the
-         * current direction.
-         */
-        cycleTime: 0,
-        /**
-         * The amount of time for which the sheep should move in the current
-         * direction before changing it.
-         */
-        cycleTimeLimit: 0,
-      },
-    };
-
-    const getDirectionTimeLimit = () => k.rand(10) * 0.2;
-
-    sheep.onStateEnter(SheepState.grazing, async () => {
-      sheepState.grazing = {
-        lastDirection: "right",
-        direction: ["left", "right", "idle"][k.rand(2)],
-        cycleTime: 0,
-        cycleTimeLimit: getDirectionTimeLimit(),
-      };
-      sheep.flipX = sheepState.grazing.direction === "left";
-      sheep.play("graze");
-    });
-
-    // prevent each sheep from colliding with other sheep
-    sheep.onCollide("sheep", () => {
-      sheepState.grazing.direction = "idle";
-      sheepState.grazing.cycleTime = 0;
-      sheepState.grazing.cycleTimeLimit = getDirectionTimeLimit();
-    });
-
-    sheep.onStateUpdate(SheepState.grazing, () => {
-      const delta = k.dt();
-
-      let moveValues: [number, number] = [-1, -1];
-      switch (sheepState.grazing.direction) {
-        case "idle": {
-          moveValues = [0, 0];
-          break;
-        }
-        case "left": {
-          moveValues = [-SHEEP_GRAZE_VELOCITY * delta, 0];
-          break;
-        }
-        case "right": {
-          moveValues = [SHEEP_GRAZE_VELOCITY * delta, 0];
-          break;
-        }
-      }
-      sheep.move(...moveValues);
-
-      sheepState.grazing.cycleTime += delta;
-
-      const shouldNotChangeDirection =
-        sheepState.grazing.cycleTime <= sheepState.grazing.cycleTimeLimit;
-      if (shouldNotChangeDirection) {
-        return;
-      }
-
-      sheepState.grazing.cycleTime = 0;
-      sheepState.grazing.cycleTimeLimit = getDirectionTimeLimit();
-
-      // if currently idle, start moving in a direction
-      if (sheepState.grazing.direction === "idle") {
-        // if the sheep was last going left, go right now
-        sheepState.grazing.direction =
-          sheepState.grazing.lastDirection === "right" ? "left" : "right";
-        // track last direction to know what direction to move in next time
-        sheepState.grazing.lastDirection = sheepState.grazing.direction;
-        // sheep sprite faces right by default - flip it to the left if it's going left
-        sheep.flipX = sheepState.grazing.direction === "left";
-        // play the grazing animation while moving
-        sheep.play("graze");
-        return;
-      }
-
-      // sheep was moving, so now should idle for a cycle
-      sheepState.grazing.direction = "idle";
-      sheep.stop();
-      sheep.frame = 0;
-    });
-
-    sheep.enterState(SheepState.grazing);
-
-    gameState.sheep[options.name] = sheep;
-
-    const props = {
-      isSelected: () => gameState.sheepSelected.has(sheep.name),
-      toggleSelected: () => {
-        const wasAlreadySelected = gameState.sheepSelected.has(sheep.name);
-
-        wasAlreadySelected
-          ? gameState.sheepSelected.delete(sheep.name)
-          : gameState.sheepSelected.add(sheep.name);
-
-        sheep.setSelected(!wasAlreadySelected);
-      },
-    };
-
-    k.onClick(sheepTag, () => {
-      props.toggleSelected();
-    });
-
-    return props;
-  }
 
   k.go(SCENES.menu);
 }
---
+++ src/sheep.ts
@@ -0,0 +1,223 @@
+import {
+  AreaComp,
+  BodyComp,
+  Comp,
+  GameObj,
+  PosComp,
+  SpriteComp,
+  StateComp,
+} from "kaboom";
+import { k } from "./kaboom";
+import { IGameState, SPRITES } from "./game";
+
+enum SheepState {
+  grazing = "grazing",
+  walking = "walking",
+}
+
+const SHEEP_ANIM_SPEED = 0.6;
+const SHEEP_GRAZE_VELOCITY = 3000;
+
+interface ICreateSheepOptions {
+  name: string;
+  pos: [number, number];
+  initialState?: SheepState;
+}
+
+export function createSheep(
+  gameState: IGameState,
+  options: ICreateSheepOptions
+) {
+  if (!options.name) {
+    throw new Error("Sheep must have a name");
+  }
+
+  if (gameState.sheep[options.name]) {
+    throw new Error(`Sheep name ${options.name} already exists`);
+  }
+
+  if (!options.pos) {
+    throw new Error("Sheep must have a position");
+  }
+
+  function selectable() {
+    let isSelected = false;
+    return {
+      id: "selectable",
+      require: ["area"],
+      draw() {
+        if (!isSelected) return;
+        k.drawRect({
+          width: 32,
+          height: 17,
+          pos: k.vec2(0, 0),
+          fill: false,
+          outline: { color: k.RED, width: 1 },
+        });
+      },
+      inspect() {
+        return String(isSelected);
+      },
+      setSelected(value: boolean) {
+        isSelected = value;
+      },
+      setType(this: GameObj, type: "bomber" | "shielder" | "commando") {
+        if (type === "bomber") {
+          this.use(k.sprite(SPRITES.sheepBomber));
+          return;
+        }
+
+        this.use(k.sprite(SPRITES.sheep));
+      },
+      toggleSelected() {
+        const wasAlreadySelected = gameState.sheepSelected.has(sheep.name);
+
+        wasAlreadySelected
+          ? gameState.sheepSelected.delete(sheep.name)
+          : gameState.sheepSelected.add(sheep.name);
+
+        this.setSelected(!wasAlreadySelected);
+      },
+    };
+  }
+
+  const getDirectionTimeLimit = () => k.rand(10) * 0.2;
+
+  const sheepTag = `sheep-${options.name}`;
+  const sheep = k.add([
+    "sheep",
+    { name: options.name },
+    sheepTag,
+    k.pos(...options.pos),
+    k.sprite(SPRITES.sheep, { animSpeed: SHEEP_ANIM_SPEED }),
+    k.scale(3, 3),
+    k.state(
+      options.initialState || SheepState.grazing,
+      Object.values(SheepState)
+    ),
+    k.area(),
+    selectable(),
+    k.body(),
+    (function (): Comp {
+      const states = {
+        isSelected: false,
+        grazing: {
+          /**
+           * The last direction the sheep was moving in.
+           * Used to determine what the next direction should be.
+           */
+          lastDirection: "",
+          /**
+           * The current direction the sheep is moving in.
+           * Can be "left", "right", or "idle".
+           */
+          direction: "right",
+          /**
+           * The amount of time for which the sheep has been moving in the
+           * current direction.
+           */
+          cycleTime: 0,
+          /**
+           * The amount of time for which the sheep should move in the current
+           * direction before changing it.
+           */
+          cycleTimeLimit: 0,
+        },
+      };
+
+      return {
+        id: "sheepState",
+        add: function (
+          this: GameObj<StateComp | SpriteComp | AreaComp | BodyComp | PosComp>
+        ) {
+          this.onStateEnter(SheepState.grazing, async () => {
+            states.grazing = {
+              lastDirection: "right",
+              direction: ["left", "right", "idle"][k.rand(2)],
+              cycleTime: 0,
+              cycleTimeLimit: getDirectionTimeLimit(),
+            };
+            this.flipX = states.grazing.direction === "left";
+            this.play("graze");
+          });
+
+          // prevent each sheep from colliding with other sheep
+          this.onCollide("sheep", () => {
+            states.grazing.direction = "idle";
+            states.grazing.cycleTime = 0;
+            states.grazing.cycleTimeLimit = getDirectionTimeLimit();
+          });
+
+          this.onStateUpdate(SheepState.grazing, () => {
+            const delta = k.dt();
+
+            let moveValues: [number, number] = [-1, -1];
+            switch (states.grazing.direction) {
+              case "idle": {
+                moveValues = [0, 0];
+                break;
+              }
+              case "left": {
+                moveValues = [-SHEEP_GRAZE_VELOCITY * delta, 0];
+                break;
+              }
+              case "right": {
+                moveValues = [SHEEP_GRAZE_VELOCITY * delta, 0];
+                break;
+              }
+            }
+            this.move(...moveValues);
+
+            states.grazing.cycleTime += delta;
+
+            const shouldNotChangeDirection =
+              states.grazing.cycleTime <= states.grazing.cycleTimeLimit;
+            if (shouldNotChangeDirection) {
+              return;
+            }
+
+            states.grazing.cycleTime = 0;
+            states.grazing.cycleTimeLimit = getDirectionTimeLimit();
+
+            // if currently idle, start moving in a direction
+            if (states.grazing.direction === "idle") {
+              // if the sheep was last going left, go right now
+              states.grazing.direction =
+                states.grazing.lastDirection === "right" ? "left" : "right";
+              // track last direction to know what direction to move in next time
+              states.grazing.lastDirection = states.grazing.direction;
+              // sheep sprite faces right by default - flip it to the left if it's going left
+              this.flipX = states.grazing.direction === "left";
+              // play the grazing animation while moving
+              this.play("graze");
+              return;
+            }
+
+            // sheep was moving, so now should idle for a cycle
+            states.grazing.direction = "idle";
+            this.stop();
+            this.frame = 0;
+          });
+
+          // start in the grazing state
+          this.enterState(SheepState.grazing);
+
+          // add the sheep to the game state
+          gameState.sheep[options.name] = this;
+        },
+      };
+    })(),
+  ]);
+
+  k.onClick(sheepTag, () => {
+    const wasAlreadySelected = gameState.sheepSelected.has(sheep.name);
+
+    wasAlreadySelected
+      ? gameState.sheepSelected.delete(sheep.name)
+      : gameState.sheepSelected.add(sheep.name);
+
+    sheep.setSelected(!wasAlreadySelected);
+  });
+
+  return sheep;
+}
